590a7c4e4c1e075578ffd50a822dc7ca35ac1aaf
DavoLobos <43319480+DavoLobos@users.noreply.github.com>
2025-04-26 10:14:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('ingreso.html')
 
 # Este if es útil para correr localmente, pero Render usará gunicorn
 if __name__ == '__main__':
