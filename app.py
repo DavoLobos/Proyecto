aa3d4f5d74def6ebefed9309191b7928f52e72ca
DavoLobos <43319480+DavoLobos@users.noreply.github.com>
2025-05-02 15:39:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,32 @@ if __name__ == '__main__':
 import os
 
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+# Esto lo sumé despues
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Definición del modelo
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+@app.before_first_request
+def crear_tablas():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return "¡App conectada y tabla creada si no existía!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
