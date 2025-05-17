9a485c8c48d1fb5a1e25280c9dec349979fd5819
DavoLobos <43319480+DavoLobos@users.noreply.github.com>
2025-05-17 11:41:39 -0300

    Create usuario.py

---
+++ models/usuario.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    correo = db.Column(db.String(120), unique=True, nullable=False)
