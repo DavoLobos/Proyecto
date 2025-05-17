11b24a1430c0cd8ab973fffb1b59cd93c9e03912
DavoLobos <43319480+DavoLobos@users.noreply.github.com>
2025-05-17 11:38:59 -0300

    Update registro_form.py

--- forms/registro_form.py
+++ forms/registro_form.py
@@ -1,1 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class RegistroForm(FlaskForm):
+    nombre = StringField('Nombre', validators=[DataRequired(), Length(min=2, max=50)])
+    correo = StringField('Correo', validators=[DataRequired(), Email(), Length(max=120)])
+    submit = SubmitField('Registrarse')
 
